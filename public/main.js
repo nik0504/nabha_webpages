cf70b92d337fd3e456beaea87158df52468b262e
nik0504 <nikhil33bansal2gmail.com>
2021-08-17 14:54:33 +0530

    compressed image

--- public/main.js
+++ public/main.js
@@ -37,12 +37,14 @@ $("#contactForm").submit(function (e) {
     $(".shopname").val() +
     $(".phoneNo").val();
 
-  console.log(ur);
+  // console.log(ur);
   var type = "shopForm/" + $(".category").val() + "/" + $(".subcategory").val();
-  console.log(type);
-  var messagesRef = firebase.database().ref(type);
+  var typeall = "allFormData/";
+  // console.log(type);
 
-  const ref = firebase.storage().ref(ur);
+  var messagesRef = firebase.database().ref(type);
+  var messagesRefAll = firebase.database().ref(typeall);
+
   const file = document.querySelector("#photo1").files[0];
   var file2 = document.querySelector("#photo2").files[0]
     ? document.querySelector("#photo2").files[0]
@@ -50,101 +52,239 @@ $("#contactForm").submit(function (e) {
   // var file3 = document.querySelector("#photo3").files[0]
   //   ? document.querySelector("#photo3").files[0]
   //   : "no";
-  console.log(file2 != "no");
+  // console.log(file2 != "no");
   // console.log(file3);
   const name = +new Date() + "-" + file.name;
   const name2 = +new Date() + "-" + "Extra";
   // const name3 = +new Date() + "-" + "Owner";
+  const ref1 = firebase.storage().ref(ur).child(name);
+  const ref2 = firebase.storage().ref(ur).child(name2);
+
   const metadata = {
     contentType: file.type,
   };
 
   if (file2 != "no") {
-    const uploadTask2 = ref.child(name2).put(file2, metadata);
-    console.log("2nd");
-    uploadTask2.on(
-      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      function (snapshot) {},
-      function (error) {
-        switch (error.code) {
-          case "storage/unauthorized":
-            break;
-          case "storage/canceled":
-            break;
-          case "storage/unknown":
-            break;
-        }
-      },
-      function () {
-        uploadTask2.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-          console.log("File available at", downloadURL);
-          localStorage.setItem("ShopImage2", downloadURL);
-          trigger();
-        });
-      }
-    );
+    var reader1 = new FileReader();
 
-    const uploadTask1 = ref.child(name).put(file, metadata);
+    // Set the image for the FileReader
+    reader1.onload = function (event) {
+      var img = document.createElement("img");
+      img.onload = function (e) {
+        // Create your canvas
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
 
-    uploadTask1.on(
-      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      function (snapshot) {},
-      function (error) {
-        switch (error.code) {
-          case "storage/unauthorized":
-            break;
-          case "storage/canceled":
-            break;
-          case "storage/unknown":
-            break;
+        var MAX_WIDTH = 500;
+        var MAX_HEIGHT = 500;
+        var width = img.width;
+        var height = img.height;
+
+        // Add the resizing logic
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
         }
-      },
-      function () {
-        // if (file2 == "no") {
-        //   localStorage.setItem("ShopImage2", "");
-        // }
-        uploadTask1.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-          console.log("File available at", downloadURL);
-          localStorage.setItem("ShopImage", downloadURL);
-          // setTimeout(function () {
-          //   i();
-          // }, 1000);
-          // i();
-          trigger();
-        });
-      }
-    );
-    // i();
+
+        //Specify the resizing result
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+        // console.log(file);
+
+        var dataurl1 = canvas.toDataURL(
+          document.querySelector("#photo1").files[0].type
+        );
+        // console.log(dataurl1);
+
+        const uploadTask1 = ref1.putString(dataurl1.toString(), "data_url");
+
+        uploadTask1.on(
+          firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+          function (snapshot) {},
+          function (error) {
+            switch (error.code) {
+              case "storage/unauthorized":
+                break;
+              case "storage/canceled":
+                break;
+              case "storage/unknown":
+                break;
+            }
+          },
+          function () {
+            // if (file2 == "no") {
+            //   localStorage.setItem("ShopImage2", "");
+            // }
+            uploadTask1.snapshot.ref
+              .getDownloadURL()
+              .then(function (downloadURL) {
+                // console.log("File available at", downloadURL);
+                localStorage.setItem("ShopImage", downloadURL);
+                trigger();
+              });
+          }
+        );
+      };
+      img.src = event.target.result;
+    };
+    reader1.readAsDataURL(document.querySelector("#photo1").files[0]);
+
+    //2nd
+
+    var reader2 = new FileReader();
+
+    // Set the image for the FileReader
+    reader2.onload = function (event) {
+      var img = document.createElement("img");
+      img.onload = function (e) {
+        // Create your canvas
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+
+        var MAX_WIDTH = 500;
+        var MAX_HEIGHT = 500;
+        var width = img.width;
+        var height = img.height;
+
+        // Add the resizing logic
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+
+        //Specify the resizing result
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+        // console.log(file);
+
+        // var dataurl1 = canvas.toDataURL(
+        //   document.querySelector("#photo1").files[0].type
+        // );
+        var dataurl2 = canvas.toDataURL(
+          document.querySelector("#photo2").files[0].type
+        );
+        // console.log(dataurl1);
+        // console.log(dataurl2);
+
+        const uploadTask2 = ref2.putString(dataurl2.toString(), "data_url");
+        // console.log("2nd");
+        uploadTask2.on(
+          firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+          function (snapshot) {},
+          function (error) {
+            switch (error.code) {
+              case "storage/unauthorized":
+                break;
+              case "storage/canceled":
+                break;
+              case "storage/unknown":
+                break;
+            }
+          },
+          function () {
+            uploadTask2.snapshot.ref
+              .getDownloadURL()
+              .then(function (downloadURL) {
+                // console.log("File available at", downloadURL);
+                localStorage.setItem("ShopImage2", downloadURL);
+                trigger();
+              });
+          }
+        );
+      };
+      img.src = event.target.result;
+    };
+    reader2.readAsDataURL(document.querySelector("#photo2").files[0]);
   } else {
-    const uploadTask1 = ref.child(name).put(file, metadata);
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var img = document.createElement("img");
+      img.onload = function (e) {
+        // Create your canvas
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
 
-    uploadTask1.on(
-      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      function (snapshot) {},
-      function (error) {
-        switch (error.code) {
-          case "storage/unauthorized":
-            break;
-          case "storage/canceled":
-            break;
-          case "storage/unknown":
-            break;
+        var MAX_WIDTH = 500;
+        var MAX_HEIGHT = 500;
+        var width = img.width;
+        var height = img.height;
+
+        // Add the resizing logic
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
         }
-      },
-      function () {
-        if (file2 == "no") {
-          localStorage.setItem("ShopImage2", "");
-        }
-        uploadTask1.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-          console.log("File available at", downloadURL);
-          localStorage.setItem("ShopImage", downloadURL);
-          // setTimeout(function () {
-          //   i();
-          // }, 1000);
-          i();
-        });
-      }
-    );
+
+        //Specify the resizing result
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+
+        var dataurl1 = canvas.toDataURL(
+          document.querySelector("#photo1").files[0].type
+        );
+        // console.log("only 1 " + dataurl1);
+        const uploadTask1 = ref1.putString(dataurl1.toString(), "data_url");
+
+        uploadTask1.on(
+          firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+          function (snapshot) {},
+          function (error) {
+            switch (error.code) {
+              case "storage/unauthorized":
+                break;
+              case "storage/canceled":
+                break;
+              case "storage/unknown":
+                break;
+            }
+          },
+          function () {
+            if (file2 == "no") {
+              localStorage.setItem("ShopImage2", "");
+            }
+            uploadTask1.snapshot.ref
+              .getDownloadURL()
+              .then(function (downloadURL) {
+                // console.log("File available at", downloadURL);
+                localStorage.setItem("ShopImage", downloadURL);
+                i();
+              });
+          }
+        );
+      };
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(document.querySelector("#photo1").files[0]);
   }
 
   function trigger() {
@@ -156,90 +296,9 @@ $("#contactForm").submit(function (e) {
     }
   }
 
-  // if (file3 != "no") {
-  //   const uploadTask3 = ref.child(name3).put(file3, metadata);
-  //   console.log("3rrrd");
-
-  //   uploadTask3.on(
-  //     firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-  //     function (snapshot) {},
-  //     function (error) {
-  //       switch (error.code) {
-  //         case "storage/unauthorized":
-  //           break;
-  //         case "storage/canceled":
-  //           break;
-  //         case "storage/unknown":
-  //           break;
-  //       }
-  //     },
-  //     function () {
-  //       uploadTask3.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-  //         console.log("File available at", downloadURL);
-  //         localStorage.setItem("ShopImage3", downloadURL);
-  //       });
-  //     }
-  //   );
-  // }
-
-  // const uploadTask1 = ref.child(name).put(file, metadata);
-
-  // uploadTask1.on(
-  //   firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-  //   function (snapshot) {},
-  //   function (error) {
-  //     switch (error.code) {
-  //       case "storage/unauthorized":
-  //         break;
-  //       case "storage/canceled":
-  //         break;
-  //       case "storage/unknown":
-  //         break;
-  //     }
-  //   },
-  //   function () {
-  //     if (file2 == "no") {
-  //       localStorage.setItem("ShopImage2", "");
-  //     }
-  //     uploadTask1.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-  //       console.log("File available at", downloadURL);
-  //       localStorage.setItem("ShopImage", downloadURL);
-  //       // setTimeout(function () {
-  //       //   i();
-  //       // }, 1000);
-  //       i();
-  //     });
-  //   }
-  // );
-  //   },
-  //   function(){
-  // var messagesRef = firebase.database().ref('contactformmessages');
-  // var newMessageRef = messagesRef.push().key;
-  // var durl= task.snapshot.databaseURL;
-  //       console.log(durl);
-  // var updates = {};
-  // var postData = {
-  //     name: $('.fullname').val(),
-  //     email: $('.email').val(),
-  //     subject: $('.subject').val(),
-  //     message: $('.message').val(),
-  //     url: durl
-  // }
-  // updates['/contactformmessages'+newMessageRef] = postData;
-  // firebase.database().ref().update(updates);
-  //       }
-  //   );
-  // uploadTask.then(snapshot => snapshot.ref.getDownloadURL())
-  //     // firebase.database().ref(ur)
-  //     .then(url => {
-  //       console.log(url);
-  //       localStorage.setItem("url", url);
-  //     })
-  //     .catch(console.error);
-
   var shopName = $(".shopname").val();
   var category = $(".category").val();
-  var subCategory = $(".subcategory").val();
+  var subcategory = $(".subcategory").val();
   var yourname = $(".yourname").val();
   var phoneNo = $(".phoneNo").val();
   var wphoneNo = $(".wphoneNo").val();
@@ -252,6 +311,7 @@ $("#contactForm").submit(function (e) {
 
   function i() {
     var newMessageRef = messagesRef.push();
+    var newMessageRefAll = messagesRefAll.push();
     var ShopI = localStorage.getItem("ShopImage");
     var ShopI2 = localStorage.getItem("ShopImage2")
       ? localStorage.getItem("ShopImage2")
@@ -259,8 +319,8 @@ $("#contactForm").submit(function (e) {
     // var ShopI3 = localStorage.getItem("ShopImage3")
     //   ? localStorage.getItem("ShopImage3")
     //   : "";
-    console.log("File available at", ShopI);
-    console.log("File available at", ShopI2);
+    // console.log("File available at", ShopI);
+    // console.log("File available at", ShopI2);
     newMessageRef.set({
       shopName: shopName,
       category: category,
@@ -282,6 +342,32 @@ $("#contactForm").submit(function (e) {
       extra1: "",
       extra2: "",
       extra3: "",
+      extra4: "",
+      extra5: "",
+    });
+    newMessageRefAll.set({
+      shopName: shopName,
+      category: category,
+      subCategory: subcategory,
+      ownerName: yourname,
+      phoneNo: phoneNo,
+      wphoneNo: wphoneNo,
+      oTime: oTime,
+      cTime: cTime,
+      address: address,
+      nearby: nearby,
+      map: map,
+      description: description,
+      ShopImage2: ShopI2.length < 1 ? [ShopI] : [ShopI, ShopI2],
+      ShopImage: ShopI,
+      // ShopImage3: ShopI3,
+      verify: false,
+      slider: [""],
+      extra1: "",
+      extra2: "",
+      extra3: "",
+      extra4: "",
+      extra5: "",
     });
     localStorage.clear();
     $(".success-message").show();
--- public/mainForWorker.js
+++ public/mainForWorker.js
@@ -24,49 +24,132 @@ $("#contactForm").submit(function (e) {
     $(".shopname").val() +
     $(".phoneNo").val();
 
-  console.log(ur);
+  // console.log(ur);
   var type = "workerForm/" + $(".category").val();
-  console.log(type);
+  // console.log(type);
   var messagesRef = firebase.database().ref(type);
 
-  const ref = firebase.storage().ref(ur);
-  const file = document.querySelector("#photo1").files[0];
+  var file = document.querySelector("#photo1").files[0];
+  // var date = new Date();
+  // console.log(date);
+
   const name = +new Date() + "-" + file.name;
+
+  const ref = firebase.storage().ref(ur).child(name);
+
+  // const name = +new Date() + "-" + file.name;
+
   // const name2 = +new Date() + "-" + "Extra";
   // const name3 = +new Date() + "-" + "Owner";
-  const metadata = {
-    contentType: file.type,
-  };
+  // const metadata = {
+  //   contentType: file.type,
+  // };
+  // console.log(file);
 
-  const uploadTask1 = ref.child(name).put(file, metadata);
+  // if (file) {
+  var reader = new FileReader();
 
-  uploadTask1.on(
-    firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function (snapshot) {},
-    function (error) {
-      switch (error.code) {
-        case "storage/unauthorized":
-          break;
-        case "storage/canceled":
-          break;
-        case "storage/unknown":
-          break;
+  // Set the image for the FileReader
+  reader.onload = function (event) {
+    var img = document.createElement("img");
+    img.onload = function (e) {
+      // Create your canvas
+      var canvas = document.createElement("canvas");
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+
+      var MAX_WIDTH = 300;
+      var MAX_HEIGHT = 300;
+      var width = img.width;
+      var height = img.height;
+
+      // Add the resizing logic
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
       }
-    },
-    function () {
-      //   if (file2 == "no") {
-      //     localStorage.setItem("ShopImage2", "");
-      //   }
-      uploadTask1.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-        console.log("File available at", downloadURL);
-        localStorage.setItem("ShopImage", downloadURL);
-        // setTimeout(function () {
-        //   i();
-        // }, 1000);
-        i();
-      });
-    }
-  );
+
+      //Specify the resizing result
+      canvas.width = width;
+      canvas.height = height;
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, width, height);
+      // console.log(file);
+
+      var dataurl = canvas.toDataURL(
+        document.querySelector("#photo1").files[0].type
+      );
+      const uploadTask1 = ref.putString(dataurl.toString(), "data_url");
+
+      uploadTask1.on(
+        firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function (snapshot) {},
+        function (error) {
+          switch (error.code) {
+            case "storage/unauthorized":
+              break;
+            case "storage/canceled":
+              break;
+            case "storage/unknown":
+              break;
+          }
+        },
+        function () {
+          uploadTask1.snapshot.ref
+            .getDownloadURL()
+            .then(function (downloadURL) {
+              // console.log("File available at", downloadURL);
+              localStorage.setItem("ShopImage", downloadURL);
+              i();
+            });
+        }
+      );
+    };
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(document.querySelector("#photo1").files[0]);
+  // }
+
+  // const name = +new Date() + "-" + file.name;
+  // // const name2 = +new Date() + "-" + "Extra";
+  // // const name3 = +new Date() + "-" + "Owner";
+  // const metadata = {
+  //   contentType: file.type,
+  // };
+
+  // const uploadTask1 = ref.child(name).put(file, metadata);
+
+  // uploadTask1.on(
+  //   firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+  //   function (snapshot) {},
+  //   function (error) {
+  //     switch (error.code) {
+  //       case "storage/unauthorized":
+  //         break;
+  //       case "storage/canceled":
+  //         break;
+  //       case "storage/unknown":
+  //         break;
+  //     }
+  //   },
+  //   function () {
+  //     //   if (file2 == "no") {
+  //     //     localStorage.setItem("ShopImage2", "");
+  //     //   }
+  //     uploadTask1.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+  //       console.log("File available at", downloadURL);
+  //       localStorage.setItem("ShopImage", downloadURL);
+  //       i();
+  //     });
+  //   }
+  // );
 
   var shopName = $(".shopname").val();
   var category = $(".category").val();
@@ -79,6 +162,7 @@ $("#contactForm").submit(function (e) {
   // var nearby = $(".nearby").val();
   // var map = $(".map").val();
   var description = $(".description").val();
+  // var date = new Date();
 
   function i() {
     var newMessageRef = messagesRef.push();
@@ -86,8 +170,10 @@ $("#contactForm").submit(function (e) {
     // var ShopI3 = localStorage.getItem("ShopImage3")
     //   ? localStorage.getItem("ShopImage3")
     //   : "";
-    console.log("File available at", ShopI);
+    // console.log("File available at", ShopI);
     //   console.log("File available at", ShopI2);
+    // const d = new Date();
+    // console.log(date);
     newMessageRef.set({
       shopName: shopName,
       category: category,
@@ -104,10 +190,13 @@ $("#contactForm").submit(function (e) {
       ShopImage: [ShopI],
       // ShopImage3: ShopI3,
       verify: false,
+      // createdOn: date.length > 0 ? date : "",
       slider: [""],
       extra1: "",
       extra2: "",
       extra3: "",
+      extra4: "",
+      extra5: "",
     });
     localStorage.clear();
     $(".success-message").show();
